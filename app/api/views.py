a3bca3fe6ccd492f704551015776daa1a61f6291
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2024-07-11 20:38:12 -0300

    feat: add explore api get

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from storage.models import CodeStorage
 
 # Create your views here.
+
+@require_http_methods(["GET"])
+def explore(request):
+    storages = CodeStorage.most_recent()
+
+    formatted_data = []
+    for storage in storages:
+        formatted_data.append({
+            'slug': storage.slug,
+            'title': storage.title,
+            'creation': storage.created.strftime("%H:%M %d/%m")
+        })
+
+    return JsonResponse({'data': formatted_data})
+
