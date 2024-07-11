84dcde36a2e04017b05fd2b4640bbf79aeaf7b2c
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2024-07-11 19:34:09 -0300

    feat: create CodeStorage model

--- app/storage/admin.py
+++ app/storage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from storage.models import CodeStorage
 
 # Register your models here.
+
+admin.site.register(CodeStorage)
--- app/storage/models.py
+++ app/storage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class CodeStorage(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    expiration = models.DateTimeField(null=True, blank=True)
+    slug = models.CharField(max_length=5)
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=25000)
+    password = models.CharField(max_length=50, null=True, blank=True)
+    active = models.BooleanField(default=True)
