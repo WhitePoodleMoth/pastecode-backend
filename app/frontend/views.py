0458dec298e82ba1e1671ed67574d6cf25628229
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2024-07-11 19:55:48 -0300

    feat: add search view & path

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from storage.models import CodeStorage
 
 # Create your views here.
+
+def search(request, slug):
+    storage = CodeStorage.search_by_slug(slug)
+    if storage:
+        return HttpResponse(storage.content)
+    else:
+        return HttpResponse("Conteudo nao encontrado.")
